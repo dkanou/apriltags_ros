467622205fe5031bd1990bba8256e42017f79159
Dimitrios Kanoulas <dimitrios.kanoulas@iit.it>
2015-03-23 19:50:15 +0100

    Working on CMU integration

--- apriltags_ros/include/apriltags_ros/apriltag_detector.h
+++ apriltags_ros/include/apriltags_ros/apriltag_detector.h
@@ -1,22 +1,61 @@
+/* Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015-, Dimitrios Kanoulas
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef APRILTAG_DETECTOR_H
 #define APRILTAG_DETECTOR_H
 
 #include <ros/ros.h>
+
+#include <AprilTags/TagDetector.h>
+#include <apriltags_ros/AprilTagDetectionArray.h>
+
 #include <sensor_msgs/PointCloud2.h>
 
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
 
+#include <cv_bridge/cv_bridge.h>
+
 #include <visualization_msgs/MarkerArray.h>
 #include <image_transport/image_transport.h>
 
-#include <AprilTags/TagDetector.h>
 #include <tf/transform_broadcaster.h>
+
 
 namespace apriltags_ros
 {
-
   class AprilTagDescription
   {
     public:
@@ -44,9 +83,11 @@ namespace apriltags_ros
       ~AprilTagDetector ();
 
     private:
+      /** \brief Image and camera info callback function. */
       void imageCb (const sensor_msgs::ImageConstPtr& msg,
                     const sensor_msgs::CameraInfoConstPtr& cam_info);
 
+      /** \brief Cloud callback function. */
       void stereoCB (const sensor_msgs::PointCloud2ConstPtr& msg);
 
       std::map<int, AprilTagDescription> parse_tag_descriptions (XmlRpc::XmlRpcValue& april_tag_descriptions);
@@ -61,21 +102,44 @@ namespace apriltags_ros
       /** \brief Stereo camera subscriber. */
       ros::Subscriber stereo_sub_;
 
-      std::map<int, AprilTagDescription> descriptions_;
+      /** \brief Sensor's frame id. */
       std::string sensor_frame_id_;
+
+      /** \brief AprilTags descriptions. */
+      std::map<int, AprilTagDescription> descriptions_;
+
+      /** \brief Tag detections vector. */
+      std::vector<AprilTags::TagDetection> detections_;
+      
+      std::map<int, AprilTagDescription>::const_iterator description_itr;
+
+      /** \brief OpenCV image. */
+      cv_bridge::CvImagePtr cv_ptr;
+
+      /** \brief Image transport. */
       image_transport::ImageTransport it_;
 
-      tf::TransformBroadcaster tf_pub_;
+      /** \brief */
       boost::shared_ptr<AprilTags::TagDetector> tag_detector_;
 
+      /** \brief */
+      AprilTagDetectionArray tag_detection_array;
+
+      /** \brief */
+      double tag_size;
+
       // Publishers
+      tf::TransformBroadcaster tf_pub_;
       image_transport::Publisher image_pub_;
       ros::Publisher detections_pub_;
       ros::Publisher pose_pub_;
 
       /** \brief Apriltag 4 corners and the center. */
-      float p_ul;
-      float cx, cy;
+      float p_ul, cx, cy;
+
+      /** \brief Camera's intrinsic parameters. */
+      double fx_, fy_, px_, py_;
+      int cam_width_, cam_height_;
 
       /** \brief The latest processed point cloud from the stereo camera. */
       pcl::PointCloud<pcl::PointXYZ>::Ptr stereo_cloud_ptr_;
--- apriltags_ros/src/apriltag_detector.cpp
+++ apriltags_ros/src/apriltag_detector.cpp
@@ -1,11 +1,45 @@
+/* Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015-, Dimitrios Kanoulas
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder(s) nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <apriltags_ros/apriltag_detector.h>
-#include <cv_bridge/cv_bridge.h>
+
 #include <sensor_msgs/image_encodings.h>
 #include <boost/foreach.hpp>
+#include <pcl-1.7/pcl/common/copy_point.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
 #include <apriltags_ros/AprilTagDetection.h>
-#include <apriltags_ros/AprilTagDetectionArray.h>
 #include <AprilTags/Tag16h5.h>
 #include <AprilTags/Tag25h7.h>
 #include <AprilTags/Tag25h9.h>
@@ -22,6 +56,7 @@ namespace apriltags_ros
     node_ (nh),
     stereo_cloud_ptr_ (new pcl::PointCloud<pcl::PointXYZ> ())
   {
+    // Parse all the AprilTags descriptions
     XmlRpc::XmlRpcValue april_tag_descriptions;
 
     if (!pnh.getParam("tag_descriptions", april_tag_descriptions))
@@ -40,17 +75,25 @@ namespace apriltags_ros
       }
     }
 
+    // Set the sensor frame
     if (!pnh.getParam("sensor_frame_id", sensor_frame_id_))
     {
-      sensor_frame_id_ = "camera_rgb_optical_frame";
+      //sensor_frame_id_ = "camera_rgb_optical_frame";
+      sensor_frame_id_ = "multisense/right_camera_optical_frame";
     }
 
+    // AprilTags tag codes (36h11) and tag detector
     AprilTags::TagCodes tag_codes = AprilTags::tagCodes36h11;
     tag_detector_= boost::shared_ptr<AprilTags::TagDetector>(new AprilTags::TagDetector(tag_codes));
-    image_sub_ = it_.subscribeCamera("/camera/rgb/image_rect_color", 1, &AprilTagDetector::imageCb, this);
-    stereo_sub_ = node_.subscribe("/camera/depth_registered/points", 1, &AprilTagDetector::stereoCB, this);
 
+    // Subscribers
+    image_sub_ = it_.subscribeCamera("/multisense/right/image_rect", 1, &AprilTagDetector::imageCb, this);
+    stereo_sub_ = node_.subscribe("/multisense/organized_image_points2", 1, &AprilTagDetector::stereoCB, this);
 
+    //image_sub_ = it_.subscribeCamera("/camera/rgb/image_rect_color", 1, &AprilTagDetector::imageCb, this);
+    //stereo_sub_ = node_.subscribe("/camera/depth_registered/points", 1, &AprilTagDetector::stereoCB, this);
+
+    // Publishers
     image_pub_ = it_.advertise("tag_detections_image", 1);
     detections_pub_ = nh.advertise<AprilTagDetectionArray>("tag_detections", 1);
     pose_pub_ = nh.advertise<geometry_msgs::PoseArray>("tag_detections_pose", 1);
@@ -60,8 +103,9 @@ namespace apriltags_ros
   ///////////////////////////////////////////////////////////////////////////////
   AprilTagDetector::~AprilTagDetector ()
   {
+    // Subscribers
     image_sub_.shutdown();
-    //cloud_sub_.shutdown();
+    stereo_sub_.shutdown();
   }
 
 
@@ -69,8 +113,6 @@ namespace apriltags_ros
   void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg,
                                  const sensor_msgs::CameraInfoConstPtr& cam_info)
   {
-    cv_bridge::CvImagePtr cv_ptr;
-
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -83,22 +125,48 @@ namespace apriltags_ros
 
     cv::Mat gray;
     cv::cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
-    std::vector<AprilTags::TagDetection>	detections = tag_detector_->extractTags(gray);
-    ROS_DEBUG("%d tag detected", (int)detections.size());
-
-    double fx = cam_info->K[0];
-    double fy = cam_info->K[4];
-    double px = cam_info->K[2];
-    double py = cam_info->K[5];
+    detections_ = tag_detector_->extractTags(gray);
+    ROS_DEBUG("%d tag detected", (int)detections_.size());
 
     if(!sensor_frame_id_.empty())
       cv_ptr->header.frame_id = sensor_frame_id_;
 
-    AprilTagDetectionArray tag_detection_array;
+    // camera intrinsic params
+    fx_ = cam_info->K[0];
+    fy_ = cam_info->K[4];
+    px_ = cam_info->K[2];
+    py_ = cam_info->K[5];
+    cam_width_ = cam_info->width;
+    cam_height_ = cam_info->height;
+
+    BOOST_FOREACH(AprilTags::TagDetection detection, detections_)
+    {
+      std::map<int, AprilTagDescription>::const_iterator description_itr = descriptions_.find(detection.id);
+
+      if(description_itr == descriptions_.end())
+      {
+        ROS_WARN_THROTTLE(10.0, "Found tag: %d, but no description was found for it", detection.id);
+        continue;
+      }
+
+      detection.draw(cv_ptr->image);
+
+      AprilTagDescription description = description_itr->second;
+      tag_size = description.size();
+    }
+
+    image_pub_.publish (cv_ptr->toImageMsg());
+
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  void
+  AprilTagDetector::stereoCB (const sensor_msgs::PointCloud2ConstPtr& msg)
+  {
     geometry_msgs::PoseArray tag_pose_array;
     tag_pose_array.header = cv_ptr->header;
 
-    BOOST_FOREACH(AprilTags::TagDetection detection, detections)
+    BOOST_FOREACH(AprilTags::TagDetection detection, detections_)
     {
       std::map<int, AprilTagDescription>::const_iterator description_itr = descriptions_.find(detection.id);
 
@@ -109,9 +177,8 @@ namespace apriltags_ros
       }
 
       AprilTagDescription description = description_itr->second;
-      double tag_size = description.size();
+      tag_size = description.size();
 
-      detection.draw(cv_ptr->image);
       std::cout << "tag size: " << tag_size << std::endl;
       std::pair<float,float> p_[4] = detection.p;
       std::pair<float,float> cxy_ = detection.cxy;
@@ -120,14 +187,24 @@ namespace apriltags_ros
       p_ul = p_[0].first;
       std::cout << "p: " << p_[0].first << std::endl;
 
-      Eigen::Matrix4d transform = detection.getRelativeTransform(tag_size, fx, fy, px, py);
+      std::cout << "cxy_stereoCB: " << std::ceil(cx) << " , " << std::ceil(cy) << std::endl;
+      pcl::fromROSMsg(*msg, *stereo_cloud_ptr_);
+      std::cout << "Size: " << stereo_cloud_ptr_->points.size() << std::endl;
+      pcl::PointXYZ p;
+      if (stereo_cloud_ptr_->points.size())
+      {
+        std::cout << "Pointcloud z distance: " << stereo_cloud_ptr_->points[std::ceil(cx)*cam_width_+std::ceil(cy)].z << std::endl;
+        p = stereo_cloud_ptr_->points[std::ceil(cy)*cam_width_+std::ceil(cx)];
+      }
+
+      Eigen::Matrix4d transform = detection.getRelativeTransform(tag_size, fx_, fy_, px_, py_);
       Eigen::Matrix3d rot = transform.block(0,0,3,3);
       Eigen::Quaternion<double> rot_quaternion = Eigen::Quaternion<double>(rot);
 
       geometry_msgs::PoseStamped tag_pose;
-      tag_pose.pose.position.x = transform(0,3);
-      tag_pose.pose.position.y = transform(1,3);
-      tag_pose.pose.position.z = transform(2,3);
+      tag_pose.pose.position.x = p.x;//transform(0,3);
+      tag_pose.pose.position.y = p.y;//transform(1,3);
+      tag_pose.pose.position.z = p.z;//transform(2,3);
       std::cout << "x,y,z: " << transform(0,3) << "," << transform(1,3) << "," << transform(2,3) << std::endl;
       tag_pose.pose.orientation.x = rot_quaternion.x();
       tag_pose.pose.orientation.y = rot_quaternion.y();
@@ -150,20 +227,9 @@ namespace apriltags_ros
                                                    description.frame_name()));
     }
 
+    // Publishers
     detections_pub_.publish (tag_detection_array);
-    image_pub_.publish (cv_ptr->toImageMsg());
     pose_pub_.publish (tag_pose_array);
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////
-  void
-  AprilTagDetector::stereoCB (const sensor_msgs::PointCloud2ConstPtr& msg)
-  {
-    std::cout << "cxy_stereoCB: " << std::ceil(cx) << " , " << std::ceil(cy) << std::endl;
-    pcl::fromROSMsg(*msg, *stereo_cloud_ptr_);
-    std::cout << "Size: " << stereo_cloud_ptr_->points.size() << std::endl;
-    if (stereo_cloud_ptr_->points.size())
-      std::cout << stereo_cloud_ptr_->points[std::ceil(cx)*640+std::ceil(cy)].z << std::endl;
   }
 
   ///////////////////////////////////////////////////////////////////////////////
